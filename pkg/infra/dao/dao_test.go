84850a63b8b603ae6f7cf52f9e57b021ac17128c
Satoshi Ota <sota.developer@gmail.com>
2021-02-01 11:07:44 +0900

    Create from going-to-go

---
+++ pkg/infra/dao/dao_test.go
@@ -0,0 +1,47 @@
+package dao
+
+import (
+	"os"
+	"testing"
+
+	"gorm.io/gorm"
+	"github.com/tanimutomo/sqlfile"
+
+	"github.com/ispec-inc/going-to-go-example/pkg/config"
+	"github.com/ispec-inc/going-to-go-example/pkg/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func TestMain(m *testing.M) {
+	config.Init()
+
+	d, cleanup, err := mysql.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	db = d
+	defer cleanup()
+
+	os.Exit(m.Run())
+}
+
+func prepareTestData(filepath string) error {
+	s := sqlfile.New()
+	if err := s.Files("./testdata/delete.sql", filepath); err != nil {
+		return err
+	}
+
+	sqlDB, err := db.DB()
+
+	if err != nil {
+		return err
+	}
+	if _, err := s.Exec(sqlDB); err != nil {
+		return err
+	}
+	return nil
+}
