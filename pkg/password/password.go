a00744132b9fd13ab97ed5cc7002734bfec4f482
Satoshi Ota <sota.developer@gmail.com>
2021-02-03 17:09:54 +0900

    [change] password.Encrypt

--- pkg/password/password.go
+++ pkg/password/password.go
@@ -1,10 +1,17 @@
 package password
 
 import (
+	"fmt"
+
+	"github.com/ispec-inc/going-to-go-example/pkg/apperror"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func Encrypt(password string) (string, error) {
-	res, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-	return string(res), err
+func Encrypt(password *string) apperror.Error {
+	res, err := bcrypt.GenerateFromPassword([]byte(*password), 10)
+	if err != nil {
+		return apperror.New(apperror.CodeError, fmt.Errorf("error password: %v", err))
+	}
+	*password = string(res)
+	return nil
 }
