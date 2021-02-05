49541f2d2f452cb2ca4be3671ec2de300859ce9c
Satoshi Ota <sota.developer@gmail.com>
2021-02-05 11:57:52 +0900

    [rm] package jwtauth

--- pkg/value/token.go
+++ pkg/value/token.go
@@ -4,19 +4,18 @@ import (
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
-	"github.com/go-chi/jwtauth"
 	"github.com/ispec-inc/going-to-go-example/pkg/apperror"
 	"github.com/ispec-inc/going-to-go-example/pkg/config"
 )
 
 func NewToken(user_id int64) (tokenString string, aerr apperror.Error) {
 	claims := jwt.MapClaims{}
-	claims["user_id"] = int(user_id)
+	claims["user_id"] = user_id
 	claims["iat"] = time.Now().Unix()
 	claims["exp"] = time.Now().Add(time.Minute * time.Duration(config.JWT.ExpireMin)).Unix()
 
-	tokenAuth := jwtauth.New("HS256", []byte(config.JWT.Secret), nil)
-	_, tokenString, err := tokenAuth.Encode(claims)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(config.JWT.Secret))
 	if err != nil {
 		return "", apperror.New(apperror.CodeError, err)
 	}
