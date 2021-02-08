ffedb99adec8209707bd2f4a521ef2ba0fb214f5
Satoshi Ota <sota.developer@gmail.com>
2021-02-08 13:30:30 +0900

    [add] TestUserDao_FindByEmail

--- pkg/infra/dao/user_test.go
+++ pkg/infra/dao/user_test.go
@@ -47,3 +47,45 @@ func TestUserDao_Find(t *testing.T) {
 		})
 	}
 }
+
+func TestUserDao_FindByEmail(t *testing.T) {
+	t.Helper()
+	d := NewUser(db)
+
+	cases := []struct {
+		name  string
+		email string
+		want  int64
+		err   bool
+	}{
+		{
+			name:  "Found",
+			email: "test@example.com",
+			want:  int64(1),
+			err:   false,
+		},
+		{
+			name:  "NotFound",
+			email: "notfound@example.com",
+			want:  int64(0),
+			err:   true,
+		},
+	}
+	for i := range cases {
+		tc := cases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			if err := prepareTestData("./testdata/user/find_by_email.sql"); err != nil {
+				t.Error(err)
+			}
+
+			opt, aerr := d.FindByEmail(tc.email)
+
+			assert.Exactly(t, tc.want, opt.ID)
+			if tc.err {
+				assert.Error(t, aerr)
+			} else {
+				assert.NoError(t, aerr)
+			}
+		})
+	}
+}
