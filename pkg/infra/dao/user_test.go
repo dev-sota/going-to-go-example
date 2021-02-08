4ad4084b21a33f0e554545d9e45cba04425688be
Satoshi Ota <sota.developer@gmail.com>
2021-02-08 13:41:38 +0900

    [add] TestUserDao_Create

--- pkg/infra/dao/user_test.go
+++ pkg/infra/dao/user_test.go
@@ -3,6 +3,8 @@ package dao
 import (
 	"testing"
 
+	"github.com/ispec-inc/going-to-go-example/pkg/domain/model"
+	"github.com/ispec-inc/going-to-go-example/pkg/infra/entity"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -81,6 +83,64 @@ func TestUserDao_FindByEmail(t *testing.T) {
 			opt, aerr := d.FindByEmail(tc.email)
 
 			assert.Exactly(t, tc.want, opt.ID)
+			if tc.err {
+				assert.Error(t, aerr)
+			} else {
+				assert.NoError(t, aerr)
+			}
+		})
+	}
+}
+
+func TestUserDao_Create(t *testing.T) {
+	t.Helper()
+	d := NewUser(db)
+
+	cases := []struct {
+		name       string
+		model      model.User
+		createdCnt int
+		err        bool
+	}{
+		{
+			name: "Created",
+			model: model.User{
+				Email:    "test2@example.com",
+				Password: "hashed_password",
+				Name:     "test_user2",
+				Age:      25,
+			},
+			createdCnt: 1,
+			err:        false,
+		},
+		{
+			name: "AlreadyExist",
+			model: model.User{
+				Email:    "test@example.com",
+				Password: "hashed_password",
+				Name:     "test_user",
+				Age:      25,
+			},
+			createdCnt: 0,
+			err:        true,
+		},
+	}
+	for i := range cases {
+		tc := cases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			if err := prepareTestData("./testdata/user/create.sql"); err != nil {
+				t.Error(err)
+			}
+
+			var before, after []entity.User
+
+			d.db.Find(&before)
+
+			aerr := d.Create(&tc.model)
+
+			d.db.Find(&after)
+
+			assert.Exactly(t, tc.createdCnt, len(after)-len(before))
 			if tc.err {
 				assert.Error(t, aerr)
 			} else {
