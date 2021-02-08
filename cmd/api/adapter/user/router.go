07a183f9a2d3e83efaae942c151fa4eed08914c4
Satoshi Ota <sota.developer@gmail.com>
2021-02-08 12:41:43 +0900

    [rm] debug code

--- cmd/api/adapter/user/router.go
+++ cmd/api/adapter/user/router.go
@@ -5,20 +5,16 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/ispec-inc/going-to-go-example/cmd/api/middleware"
-	"github.com/ispec-inc/going-to-go-example/pkg/presenter"
 )
 
 func NewRouter(handler handler, middleware middleware.Auth) http.Handler {
 	r := chi.NewRouter()
-	r.Get("/{id}", handler.Get)
 	r.Post("/", handler.Add)
 	r.Post("/login", handler.Login)
 
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.VerifyToken)
-		r.Get("/restrict", func(w http.ResponseWriter, r *http.Request) {
-			presenter.Success(w)
-		})
+		r.Get("/{id}", handler.Get)
 	})
 
 	return r
